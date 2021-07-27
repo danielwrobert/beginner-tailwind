f84ffee8ee2f48a66f64c4f0b5a6feb50af6a5b9
Daniel W. Robert <danielwrobert@gmail.com>
2021-07-26 23:12:16 -0400

    Purge unused classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./*.html'], // see https://tailwindcss.com/docs/installation#building-your-css
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
