3eace082e14e78468107d539a77438b1193bdfc4
Daniel W. Robert <danielwrobert@gmail.com>
2021-07-29 22:35:56 -0400

    Add custom colors to Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: ['./*.html'], // see https://tailwindcss.com/docs/installation#building-your-css
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./*.html'], // see https://tailwindcss.com/docs/installation#building-your-css
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				orange: colors.orange,
+				teal: colors.teal,
+				'light-blue': colors.lightBlue,
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
